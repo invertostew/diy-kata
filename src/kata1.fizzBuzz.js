9c23adc8553c7ca8336c1db2271be6fc0853018e
Stuart Green <github.47a8z@aleeas.com>
2021-12-02 13:05:51 +0000

    fizzbuzz kata + test

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,8 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+  if (number % 15 === 0) return "FizzBuzz";
+  if (number % 5 === 0) return "Buzz";
+  if (number % 3 === 0) return "Fizz";
+  return number;
+};
 
 module.exports = fizzBuzz;
--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,23 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  test("returns Fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
+    expect(fizzBuzz(12)).toBe('Fizz');
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  test("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+    expect(fizzBuzz(25)).toBe('Buzz');
+  });
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  test("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+    expect(fizzBuzz(90)).toBe('FizzBuzz');
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  test("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(7)).toBe(7);
+    expect(fizzBuzz(23)).toBe(23);
+  });
 });
