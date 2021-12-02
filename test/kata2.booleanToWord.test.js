5317e16722558db4dbf008344e526d38dae1e824
Stuart Green <github.47a8z@aleeas.com>
2021-12-02 13:12:49 +0000

    booleanToWord + test

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,10 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  test("return 'Yes' when booleanToWord is passed true", () => {
+    expect(booleanToWord(true)).toEqual('Yes');
+  });
+  test("return 'No' when booleanToWord is passed false", () => {
+    expect(booleanToWord(false)).toEqual('No');
+  });
 });
