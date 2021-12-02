5e2b03d87a3e8c066cc90ab4e932fcae97a37227
Stuart Green <github.47a8z@aleeas.com>
2021-12-02 14:41:42 +0000

    reachDestination + test

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const timeToDestination = Math.ceil((distance / speed) * 2) / 2;
+  return `I should be there in ${timeToDestination} hours.`;
+};
 
 module.exports = reachDestination;
