0fc4682b2bbd6b703e8fede83e0fa850ae2b095a
Stuart Green <github.47a8z@aleeas.com>
2021-12-02 14:23:56 +0000

    humanCatDogYears + test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,22 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+  const humanYears = [number];
+  let catYears = number * 4;
+  let dogYears = number * 5;
+
+  if (number >= 1) {
+    catYears += 11;
+    dogYears += 10;
+  }
+
+  if (number >= 2) {
+    catYears += 5;
+    dogYears += 4;
+  }
+
+  humanYears.push(catYears);
+  humanYears.push(dogYears);
+
+  return humanYears;
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,13 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  test("returns an array", () => {
+    expect(Array.isArray(humanCatDogYears(10))).toBe(true);
+  });
+  test("returns an array of human, cat, and dog years", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+    expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+  });
+});
