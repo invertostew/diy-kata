3a37ca5366d30499f26f488b3c77e8bc349c3d77
Stuart Green <github.47a8z@aleeas.com>
2021-12-02 15:52:44 +0000

    joinNames and test

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,10 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  const stringOfNames = namesObj.reduce((accumulator, person, index) => {
+    if (index !== namesObj.length - 1) return `${accumulator}, ${person.name}`;
+    return `${accumulator} & ${person.name}`;
+  }, "");
+
+  return stringOfNames.slice(2);
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,37 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  const names = [
+    {
+      name: "Bart"
+    },
+    {
+      name: "Lisa"
+    },
+    {
+      name: "Maggie"
+    }
+  ];
+
+  const moreNames = [
+    {
+      name: "Bart"
+    },
+    {
+      name: "Lisa"
+    },
+    {
+      name: "Maggie"
+    },
+    {
+      name: "Marge"
+    },
+    {
+      name: "Homer"
+    }
+  ];
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames(names)).toEqual("Bart, Lisa & Maggie");
+    expect(joinNames(moreNames)).toEqual("Bart, Lisa, Maggie, Marge & Homer");
+  });
 });
