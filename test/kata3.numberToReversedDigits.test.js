19b0f9449048afca3033f9c063795dae2ba196ef
Stuart Green <github.47a8z@aleeas.com>
2021-12-02 13:31:43 +0000

    numberToReversedDigits + tests

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,12 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns an array", () => {
+    expect(Array.isArray(numberToReversedDigits(12345))).toBe(true);
+  });
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(12345)).toEqual([5, 4, 3, 2, 1]);
+    expect(numberToReversedDigits(91237)).toEqual([7, 3, 2, 1, 9]);
+    expect(numberToReversedDigits(10249)).toEqual([9, 4, 2, 0, 1]);
+  });
 });
