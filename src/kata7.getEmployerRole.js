dd0c4d8642fe73571468c7f2798a142a3f7d2cf1
Stuart Green <github.47a8z@aleeas.com>
2021-12-02 16:06:15 +0000

    employee role function + test

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,6 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  // Seeing as there is only one of each name...
+  return employees.find(employee => employee.name === employeeName).role;
+};
 
 module.exports = getEmployerRole;
